7dc824e496c5efee9743dea89ce7c90743186ede
Prince9193 <148461421+Prince9193@users.noreply.github.com>
2025-02-15 09:46:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+// alert("hello world");
+
+// console.log("coding is running...")
+
+
+// var a = prompt("enter your number")
+
+
+// var isTrue = confirm("Are you sure you want to leave this page")
+
+// if (isTrue){
+//     console.log("Computer is blasting")
+// }
+
+// else{
+    
+//     console.log("computer is not blasting")
+// }
+
+
+// console.log("your number is " + a)
+
+
+let a = 1;
+
+for (let i = 0; i < 1000000; i++) {
+   console.log(a+i);
+    
+}
+
+
+
+
